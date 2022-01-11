a4ed099da48b05f46688f0814eeb8969e0edc3e7
rampradyumnarampalli <rampradyumnarampalli@gmail.com>
2022-01-11 15:40:50 +0000

    corrected build and push files

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export tag=$(git log -1 --format=%h)
-docker build -t project:$tag .
+docker build -t rampradyumnarampalli/nodejs-sample:$tag .
--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+export tag=$(git log -1 --format=%h)
 docker push rampradyumnarampalli/nodejs-sample:$tag
