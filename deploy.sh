c4f1de53a2250a5bf8b97fc253fb516a89110957
rampradyumnarampalli <rampradyumnarampalli@gmail.com>
2022-01-11 16:16:26 +0000

    changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export tag=$(git log -1 --format=%h)
-kubectl set image deployment/nodejs nodejs=rampradyumnarampalli/nodejs-sample:$tag
+kubectl set image deploy nodejs nodejs=rampradyumnarampalli/nodejs-sample:$tag
