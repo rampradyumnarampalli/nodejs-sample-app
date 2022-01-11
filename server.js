8a7bcd2c9ba6ba4f20930edd0f4c32aa550bdd57
rampradyumnarampalli <rampradyumnarampalli@gmail.com>
2022-01-11 05:57:13 +0000

    changes to server.js file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Manasa!! How are you?');
+  res.send('Hello Manasa!! How are you doing?');
 });
 
 app.listen(PORT, HOST);
