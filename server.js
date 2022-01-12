18aa702f4912c2216a25f99762199e52ed7fd040
rampradyumnarampalli <rampradyumnarampalli@gmail.com>
2022-01-12 08:10:09 +0000

    changes to deploy.sh

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Ram PR, This is the updated image pushed from Jenkins');
+  res.send('Hello Ram Pradyumna, This is the updated image pushed from Jenkins');
 });
 
 app.listen(PORT, HOST);
